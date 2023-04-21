ae98d1f9221ecbbd7b0b6be467e5b3e77653bb83
hue <developer@quickytools.com>
2023-04-21 18:14:41 -0400

    Hide keyboard when lists are scrolled

--- core/ui/src/main/java/com/crisiscleanup/core/ui/KeyboardUtil.kt
+++ core/ui/src/main/java/com/crisiscleanup/core/ui/KeyboardUtil.kt
@@ -1,26 +1,19 @@
 package com.crisiscleanup.core.ui
 
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.ime
-import androidx.compose.runtime.*
-import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalFocusManager
 
 @Composable
-private fun keyboardAsState(): State<Boolean> {
-    val isImeVisible = WindowInsets.ime.getBottom(LocalDensity.current) > 0
-    return rememberUpdatedState(isImeVisible)
-}
-
-@Composable
 fun rememberCloseKeyboard(rememberKey: Any): () -> Unit {
-    val isKeyboardOpen by keyboardAsState()
     val focusManager = LocalFocusManager.current
+    // val isKeyboardOpen by rememberUpdatedState(WindowInsets.isImeVisible)
     return remember(rememberKey) {
         {
-            if (isKeyboardOpen) {
-                focusManager.clearFocus(true)
-            }
+            // TODO Only clear when keyboard is open
+            //      Currently state does not reflect visibility when inside remember
+            // Log.w("keyboard", "Is open? $isKeyboardOpen")
+            focusManager.clearFocus(true)
         }
     }
 }
