2b7c0ba11c335fa56f1fee857f1c45918fe3d260
hue <developer@quickytools.com>
2023-05-18 09:23:04 -0400

    Edit Case section slider goes to first section when scrolled to incident item

--- feature/caseeditor/src/main/java/com/crisiscleanup/feature/caseeditor/ui/CaseEditorScreen.kt
+++ feature/caseeditor/src/main/java/com/crisiscleanup/feature/caseeditor/ui/CaseEditorScreen.kt
@@ -4,11 +4,37 @@ import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,15 +51,30 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.crisiscleanup.core.commonassets.getDisasterIcon
-import com.crisiscleanup.core.designsystem.component.*
+import com.crisiscleanup.core.designsystem.component.AnimatedBusyIndicator
+import com.crisiscleanup.core.designsystem.component.BusyButton
+import com.crisiscleanup.core.designsystem.component.BusyIndicatorFloatingTopCenter
+import com.crisiscleanup.core.designsystem.component.CrisisCleanupTextButton
+import com.crisiscleanup.core.designsystem.component.TopAppBarSingleAction
+import com.crisiscleanup.core.designsystem.component.cancelButtonColors
 import com.crisiscleanup.core.designsystem.icon.CrisisCleanupIcons
-import com.crisiscleanup.core.designsystem.theme.*
+import com.crisiscleanup.core.designsystem.theme.incidentDisasterContainerColor
+import com.crisiscleanup.core.designsystem.theme.incidentDisasterContentColor
+import com.crisiscleanup.core.designsystem.theme.listItemHeight
+import com.crisiscleanup.core.designsystem.theme.listItemHorizontalPadding
+import com.crisiscleanup.core.designsystem.theme.listItemPadding
+import com.crisiscleanup.core.designsystem.theme.listItemSpacedBy
+import com.crisiscleanup.core.designsystem.theme.separatorColor
 import com.crisiscleanup.core.model.data.EmptyIncident
 import com.crisiscleanup.core.model.data.Incident
 import com.crisiscleanup.core.ui.rememberCloseKeyboard
 import com.crisiscleanup.core.ui.scrollFlingListener
-import com.crisiscleanup.feature.caseeditor.*
+import com.crisiscleanup.feature.caseeditor.CaseEditorUiState
+import com.crisiscleanup.feature.caseeditor.CaseEditorViewModel
+import com.crisiscleanup.feature.caseeditor.CasePropertyDataEditor
+import com.crisiscleanup.feature.caseeditor.ExistingWorksiteIdentifier
 import com.crisiscleanup.feature.caseeditor.R
+import com.crisiscleanup.feature.caseeditor.WorksiteSection
 import com.crisiscleanup.feature.caseeditor.model.FormFieldsInputData
 import kotlinx.coroutines.launch
 import com.crisiscleanup.core.common.R as commonR
@@ -186,7 +227,9 @@ private fun OnContentScrollRest(
                     }
                 } ?: return@forEachIndexed
             }
-            val sliderIndex = if (actualItemIndex < indexLookups.maxItemIndex) {
+            val sliderIndex = if (actualItemIndex == 0) {
+                0
+            } else if (actualItemIndex < indexLookups.maxItemIndex) {
                 indexLookups.itemSection[actualItemIndex] ?: -1
             } else {
                 indexLookups.maxSectionIndex
