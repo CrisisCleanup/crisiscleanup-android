986035c0cc06b5a03c03bf459bea79d2dd4bff25
hue <developer@quickytools.com>
2023-06-05 14:09:53 -0400

    Remove unnecessary code module

--- feature/settings/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("nowinandroid.android.feature")
-    id("nowinandroid.android.library.compose")
-    id("nowinandroid.android.library.jacoco")
-}
-
-android {
-    namespace = "com.crisiscleanup.feature.settings"
-}
