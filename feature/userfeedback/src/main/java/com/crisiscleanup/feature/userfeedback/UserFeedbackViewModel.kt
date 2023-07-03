9f4684cb2112d21b21a86a6c9f19afe44d056750
hue <developer@quickytools.com>
2023-07-03 17:04:17 -0400

    Query and pass account ID into feedback form

--- feature/userfeedback/src/main/java/com/crisiscleanup/feature/userfeedback/UserFeedbackViewModel.kt
+++ feature/userfeedback/src/main/java/com/crisiscleanup/feature/userfeedback/UserFeedbackViewModel.kt
@@ -2,13 +2,24 @@ package com.crisiscleanup.feature.userfeedback
 
 import android.net.Uri
 import android.webkit.ValueCallback
+import android.webkit.WebView
 import androidx.lifecycle.ViewModel
+import com.crisiscleanup.core.data.repository.AccountDataRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
-class UserFeedbackViewModel @Inject constructor() : ViewModel() {
+class UserFeedbackViewModel @Inject constructor(
+    accountDataRepository: AccountDataRepository,
+) : ViewModel() {
     private var fileCallback: ValueCallback<Array<Uri>>? = null
+
+    val accountId = accountDataRepository.accountData.map { it.id }
+
+    init {
+        WebView.setWebContentsDebuggingEnabled(true);
+    }
 
     fun onSaveFileCallback(callback: ValueCallback<Array<Uri>>?) {
         fileCallback = callback
--- feature/userfeedback/src/main/java/com/crisiscleanup/feature/userfeedback/ui/UserFeedbackScreen.kt
+++ feature/userfeedback/src/main/java/com/crisiscleanup/feature/userfeedback/ui/UserFeedbackScreen.kt
@@ -10,14 +10,20 @@ import android.webkit.WebView
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.crisiscleanup.core.designsystem.LocalAppTranslator
+import com.crisiscleanup.core.designsystem.component.BusyIndicatorFloatingTopCenter
 import com.crisiscleanup.core.designsystem.component.TopAppBarBackAction
 import com.crisiscleanup.feature.userfeedback.UserFeedbackViewModel
 
@@ -31,38 +37,45 @@ fun UserFeedbackRoute(
     val context = LocalContext.current
     Column {
         TopAppBarBackAction(
-            title = com.crisiscleanup.core.designsystem.LocalAppTranslator.current.translator("nav.feedback"),
+            title = LocalAppTranslator.current.translator("nav.feedback"),
             onAction = onBack,
         )
 
-        val selectImageLauncher = rememberLauncherForActivityResult(
-            contract = ActivityResultContracts.PickVisualMedia()
-        ) { uri: Uri? ->
-            uri?.let {
-                viewModel.onMediaSelected(uri)
+        val accountId by viewModel.accountId.collectAsStateWithLifecycle(0)
+        if (accountId > 0) {
+            val selectImageLauncher = rememberLauncherForActivityResult(
+                contract = ActivityResultContracts.PickVisualMedia()
+            ) { uri: Uri? ->
+                uri?.let {
+                    viewModel.onMediaSelected(uri)
+                }
             }
-        }
-        val onChooseFileCallback = remember(viewModel) {
-            { callback: ValueCallback<Array<Uri>>? ->
-                viewModel.onSaveFileCallback(callback)
-                selectImageLauncher.launch(
-                    PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
-                )
-            }
-        }
-
-        AndroidView(
-            modifier = Modifier.weight(1f),
-            factory = {
-                WebView(context).apply {
-                    settings.javaScriptEnabled = true
-                    loadUrl("file:///android_res/raw/user_feedback_form.html")
-                    webChromeClient = getChromeClient(
-                        onChooseFileCallback,
-                        true,
+            val onChooseFileCallback = remember(viewModel) {
+                { callback: ValueCallback<Array<Uri>>? ->
+                    viewModel.onSaveFileCallback(callback)
+                    selectImageLauncher.launch(
+                        PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
                     )
                 }
-            })
+            }
+
+            AndroidView(
+                modifier = Modifier.weight(1f),
+                factory = {
+                    WebView(context).apply {
+                        settings.javaScriptEnabled = true
+                        loadUrl("file:///android_res/raw/user_feedback_form.html?accountCcid=$accountId")
+                        webChromeClient = getChromeClient(
+                            onChooseFileCallback,
+                            true,
+                        )
+                    }
+                })
+        } else {
+            Box(Modifier.fillMaxSize()) {
+                BusyIndicatorFloatingTopCenter(true)
+            }
+        }
     }
 }
 
