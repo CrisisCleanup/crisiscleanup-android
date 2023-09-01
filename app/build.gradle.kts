2e6e0449da7831ea2834948a36e14f4aec30d1fb
hue <developer@quickytools.com>
2023-09-01 01:40:34 -0400

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 android {
     defaultConfig {
-        val buildVersion = 139
+        val buildVersion = 140
         applicationId = "com.crisiscleanup"
         versionCode = buildVersion
         versionName = "0.5.${buildVersion - 136}"
