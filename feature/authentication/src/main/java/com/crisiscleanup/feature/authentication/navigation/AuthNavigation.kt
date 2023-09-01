017d5c7c36efa0186dedb8c02a575b1b386b3b7b
Deep Panchal <deep302001@gmail.com>
2023-09-01 16:44:30 -0500

    refactor(auth): remove unused AuthRoute import in AuthNavigation.kt

--- feature/authentication/src/main/java/com/crisiscleanup/feature/authentication/navigation/AuthNavigation.kt
+++ feature/authentication/src/main/java/com/crisiscleanup/feature/authentication/navigation/AuthNavigation.kt
@@ -5,7 +5,6 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.crisiscleanup.core.appnav.RouteConstant.authGraphRoutePattern
 import com.crisiscleanup.core.appnav.RouteConstant.authRoute
-import com.crisiscleanup.feature.authentication.ui.AuthRoute
 import com.crisiscleanup.feature.authentication.ui.RootAuthRoute
 
 fun NavGraphBuilder.authGraph(
