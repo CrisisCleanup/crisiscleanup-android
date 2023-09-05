b124444ea7ae540b012d45ac427ea69d1b1ceda0
Deep Panchal <deep302001@gmail.com>
2023-09-05 13:39:13 -0500

    feat(auth): add survivor info header and info on SurvivorInfoScreen

--- feature/authentication/src/main/java/com/crisiscleanup/feature/authentication/ui/SurvivorInfoScreen.kt
+++ feature/authentication/src/main/java/com/crisiscleanup/feature/authentication/ui/SurvivorInfoScreen.kt
@@ -1,13 +1,19 @@
 package com.crisiscleanup.feature.authentication.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.ui.unit.dp
 import com.crisiscleanup.core.designsystem.LocalAppTranslator
 import com.crisiscleanup.core.designsystem.component.TopAppBarBackAction
-import com.crisiscleanup.feature.authentication.AuthenticationViewModel
+import com.crisiscleanup.core.designsystem.theme.LocalFontStyles
+import com.crisiscleanup.core.designsystem.theme.listItemModifier
+import com.crisiscleanup.core.designsystem.theme.primaryBlueOneTenthColor
 
 
 @Composable
@@ -27,12 +33,27 @@ fun SurvivorInfoRoute(
 private fun SurvivorInfoScreen(
     onBack: () -> Unit = {},
 ) {
-    val translator = LocalAppTranslator.current
-
+    val t = LocalAppTranslator.current
     Column {
         TopAppBarBackAction(
-            title = translator("~~I need help"),
+            title = t("~~I need help"),
             onAction = onBack,
         )
+        Column(
+            modifier = Modifier
+                .background(primaryBlueOneTenthColor)
+                .padding(8.dp),
+        ) {
+            Text(
+                modifier = listItemModifier,
+                text = t("survivor.info_for_survivors"),
+                style = LocalFontStyles.current.header1,
+            )
+            Text(
+                modifier = listItemModifier,
+                text = t("survivor.intro"),
+                style = MaterialTheme.typography.bodyLarge,
+            )
+        }
     }
 }
