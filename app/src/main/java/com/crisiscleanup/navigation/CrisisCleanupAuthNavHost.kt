3be9f84e4d97f9423fe4d1b601469e50aca56f21
hue <developer@quickytools.com>
2024-01-04 09:50:45 -0500

    Return to auth before navigating to login with email

--- app/src/main/java/com/crisiscleanup/navigation/CrisisCleanupAuthNavHost.kt
+++ app/src/main/java/com/crisiscleanup/navigation/CrisisCleanupAuthNavHost.kt
@@ -3,8 +3,10 @@ package com.crisiscleanup.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import com.crisiscleanup.core.appnav.RouteConstant
 import com.crisiscleanup.core.appnav.RouteConstant.authGraphRoutePattern
 import com.crisiscleanup.feature.authentication.navigation.authGraph
 import com.crisiscleanup.feature.authentication.navigation.emailLoginLinkScreen
@@ -25,6 +27,13 @@ import com.crisiscleanup.feature.authentication.navigation.requestAccessScreen
 import com.crisiscleanup.feature.authentication.navigation.resetPasswordScreen
 import com.crisiscleanup.feature.authentication.navigation.volunteerOrgScreen
 
+private fun NavController.popToAuth() {
+    popBackStack()
+    while (currentBackStackEntry?.destination?.route?.let { it != RouteConstant.authRoute } == true) {
+        popBackStack()
+    }
+}
+
 @Composable
 fun CrisisCleanupAuthNavHost(
     navController: NavHostController,
@@ -35,7 +44,12 @@ fun CrisisCleanupAuthNavHost(
     startDestination: String = authGraphRoutePattern,
 ) {
     val navToLoginWithEmail =
-        remember(navController) { { navController.navigateToLoginWithEmail() } }
+        remember(navController) {
+            {
+                navController.popToAuth()
+                navController.navigateToLoginWithEmail()
+            }
+        }
     val navToLoginWithPhone =
         remember(navController) { { navController.navigateToLoginWithPhone() } }
     val navToVolunteerOrg = remember(navController) { { navController.navigateToVolunteerOrg() } }
