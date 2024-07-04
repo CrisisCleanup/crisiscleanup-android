46269b30e27ce437e3e55a711bf2630786209c8b
Aaron Titus <aaron@crisiscleanup.org>
2024-07-03 22:45:23 -0600

    Lists localizations

--- feature/lists/src/main/kotlin/com/crisiscleanup/feature/crisiscleanuplists/ui/ViewListScreen.kt
+++ feature/lists/src/main/kotlin/com/crisiscleanup/feature/crisiscleanuplists/ui/ViewListScreen.kt
@@ -133,7 +133,7 @@ internal fun ViewListRoute(
         if (changeIncidentConfirmMessage.isNotBlank()) {
             val closeDialog = viewModel::clearChangeIncident
             CrisisCleanupAlertDialog(
-                title = t("~~Confirm change Incident"),
+                title = t("list.confirm_change_incident"),
                 text = changeIncidentConfirmMessage,
                 onDismissRequest = closeDialog,
                 confirmButton = {
@@ -165,7 +165,8 @@ private fun ListDetailsView(
 
     if (objectData.isEmpty()) {
         Text(
-            t("~~This list is not supported by the app or has no items."),
+            t("list.unsupported_list_explanation"),
+            .replace("{list_name}", explainSupportList.name),
             listItemModifier,
         )
     } else {
@@ -246,7 +247,7 @@ private fun ListDetailsView(
 
                 else -> {
                     item {
-                        Text(t("~~This list is not supported by the app."))
+                        Text(t("list.not_supported_by_app"))
                     }
                 }
             }
@@ -261,7 +262,7 @@ private fun MissingItem() {
         contentAlignment = Alignment.CenterStart,
     ) {
         Text(
-            LocalAppTranslator.current("~~Missing list data."),
+            LocalAppTranslator.current("list.missing_list_data"),
         )
     }
 }
@@ -391,8 +392,9 @@ private fun LazyListScope.worksiteItems(
                 contentAlignment = Alignment.CenterStart,
             ) {
                 Text(
-                    LocalAppTranslator.current("~~Case {case_number} is not under this Incident.")
+                    LocalAppTranslator.current("list.cannot_access_case_wrong_incident")
                         .replace("{case_number}", worksite.caseNumber),
+                        .replace("{incident_name}", incident.shortName),
                 )
             }
         } else {
