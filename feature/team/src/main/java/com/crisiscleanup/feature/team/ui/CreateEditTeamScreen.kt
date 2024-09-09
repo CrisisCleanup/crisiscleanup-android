924beadf542206146eb88bd62cfcd54991848f03
hue <developer@quickytools.com>
2024-09-09 17:22:42 -0400

    Navigate to team editor with parameters

--- feature/team/src/main/java/com/crisiscleanup/feature/team/ui/CreateEditTeamScreen.kt
+++ feature/team/src/main/java/com/crisiscleanup/feature/team/ui/CreateEditTeamScreen.kt
@@ -2,10 +2,13 @@ package com.crisiscleanup.feature.team.ui
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.crisiscleanup.feature.team.CreateEditTeamViewModel
 
 @Composable
 fun CreateEditTeamRoute(
     onBack: () -> Unit,
+    viewModel: CreateEditTeamViewModel = hiltViewModel(),
 ) {
-    Text("Create/edit team")
+    Text("Create/edit team ${viewModel.startingEditorStep}")
 }
