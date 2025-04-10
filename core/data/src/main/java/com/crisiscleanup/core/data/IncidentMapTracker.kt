23d623ca90cabce20e0f8783451161f36a071b2a
hue <developer@quickytools.com>
2025-04-10 16:04:01 -0400

    Use saved map location as proxy for GPS location when lacking permissions

--- core/data/src/main/java/com/crisiscleanup/core/data/IncidentMapTracker.kt
+++ core/data/src/main/java/com/crisiscleanup/core/data/IncidentMapTracker.kt
@@ -1,14 +1,10 @@
 package com.crisiscleanup.core.data
 
-import com.crisiscleanup.core.common.di.ApplicationScope
 import com.crisiscleanup.core.datastore.LocalAppPreferencesDataSource
 import com.crisiscleanup.core.model.data.EmptyIncident
 import com.crisiscleanup.core.model.data.IncidentMapCoordinates
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.mapLatest
-import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -16,13 +12,12 @@ interface IncidentMapTracker {
     /**
      * Incident ID, latitude, longitude
      */
-    val lastLocation: StateFlow<IncidentMapCoordinates>
+    val lastLocation: Flow<IncidentMapCoordinates>
 }
 
 @Singleton
 class AppIncidentMapTracker @Inject constructor(
     appPreferences: LocalAppPreferencesDataSource,
-    @ApplicationScope externalScope: CoroutineScope,
 ) : IncidentMapTracker {
     override val lastLocation = appPreferences.userData
         .mapLatest {
@@ -30,7 +25,7 @@ class AppIncidentMapTracker @Inject constructor(
             var latitude = 0.0
             var longitude = 0.0
             with(it.casesMapBounds) {
-                if (south < north && west < east) {
+                if (incidentId > 0 && south < north && west < east) {
                     latitude = (south + north) * 0.5
                     longitude = (west + east) * 0.5
                     if (latitude in -90.0..90.0 && longitude in -180.0..180.0) {
@@ -47,13 +42,4 @@ class AppIncidentMapTracker @Inject constructor(
                 )
             }
         }
-        .stateIn(
-            externalScope,
-            initialValue = IncidentMapCoordinates(
-                EmptyIncident.id,
-                0.0,
-                0.0,
-            ),
-            started = SharingStarted.WhileSubscribed(),
-        )
 }
