3a074f6c74cb68dddd2a125ff5294043a82698d1
hue <developer@quickytools.com>
2025-04-15 13:06:31 -0400

    Test parsing API error responses

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
 android {
     defaultConfig {
-        val buildVersion = 245
+        val buildVersion = 246
         applicationId = "com.crisiscleanup"
         versionCode = buildVersion
         versionName = "0.9.${buildVersion - 168}"
